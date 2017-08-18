a7f8e17519c122e727d4c0ab9e67d5282d9e9e7c
nelsonabieno <nelsonabieno@gmail.com>
2017-08-18 00:14:00 -0700

    add a todo/id route

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,42 @@
+const {ObjectID}= require('mongodb');
+
+const {mongoose} = require('./../server/db/mongoose');
+const {Todos} = require('./../server/models/Todos');
+const {Users} = require('./../server/models/Users');
+
+var id='59952fd314415c1b4c59297c';
+var userId= '599217a8d5b1d4119cf979b0';
+// if(!ObjectId.isValid(iD)){
+//     console.log('Invalid Id'); 
+// }
+// //find all Todo that belong this Id
+// Todos.find({
+//    _id: id
+// }).then((todos)=>{
+// console.log('Todos',todos);
+// });
+// //find one Todo that belong this Id
+// Todos.findOne({
+//  _id: id
+// }).then((todo)=>{
+// console.log('Todo',todo);
+// });
+
+//finds Todo by Id
+Todos.findById(id).then((todos)=>{
+ if(!todos){
+     return console.log('unable to find User')
+ }   
+console.log('Todos',todos);
+});
+
+Users.findById({
+    _id: userId
+}).then((user)=>{
+if(!user){
+    return console.log('Unable to fin user');
+}
+console.log(JSON.stringify(user, undefined,2))
+}, (e)=>{
+    console.log(e);
+})
--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 let mongoose= require('./db/mongoose');
+let {ObjectID} = require('mongodb');
 let {Todos} = require('./models/Todos');
 let {Users} = require('./models/Users');
 
@@ -30,6 +31,24 @@ res.send({todo});
 console.log('unable to display database');
 });
 });
+
+//routes to fetch Todo by id
+app.get('/todos/:id', (req,res)=>{
+    let id =req.params.id;
+    if (!ObjectID.isValid(id)){
+    return res.status(400).send('{Invalid Id }');
+         }
+         
+    Todos.findById(id).then((todo)=>{
+        if(!todo){
+        return res.status(404).send({});
+        }
+       res.status(200).send(todo);
+    },(e)=>{
+        console.log(e)
+    })
+});    
+
 
 app.listen(3000, ()=>{
     console.log('Server is running on port 3000')
